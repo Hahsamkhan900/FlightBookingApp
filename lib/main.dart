32f0761e442df4986971e657f77ae363be263b90
Hahsamkhan900 <khasham476@gmail.com>
2022-12-23 00:39:04 +0500

    Fix Debug Banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
